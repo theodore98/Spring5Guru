679581c14e4bde3332241eea722611b908fa041a
dev global <tnaveendesilva@gmail.com>
2022-04-20 19:49:55 +0530

    Initial Commit

---
+++ src/test/java/edu/firstwebapp/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.firstwebapp.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
