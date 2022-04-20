bccbc900c84a5e8b51a72a1432b9232eaac05138
dev global <tnaveendesilva@gmail.com>
2022-04-20 23:10:12 +0530

    Section 2 - 2nd Commit (Publisher Mapping)

--- src/main/java/edu/firstwebapp/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/edu/firstwebapp/spring5webapp/bootstrap/BootStrapData.java
@@ -25,33 +25,41 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher publisher = new Publisher("Publis ","London");
+        publisherRepository.save(publisher);
+
         Author eric =  new Author("Eric","Evans");
         Book ddd = new Book("Domain Driven Design","A123456");
-        Publisher pub1 = new Publisher("Publis ","London");
-
-
-
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
-        pub1.getBooks().add(ddd);
 
-        ddd.setPublishers(pub1);
+        ddd.setPublishers(publisher);
+        publisher.getBooks().add(ddd);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
-        publisherRepository.save(pub1);
+        publisherRepository.save(publisher);
+
 
         Author rod = new Author("Rod","Johnson");
         Book noEJB = new Book("J2EE","561565");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublishers(publisher);
+        publisher.getBooks().add(noEJB);
+
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
+
 
         System.out.println("Bootstrap Started");
         System.out.println("Number of Books"+bookRepository.count());
-        System.out.println("Number of Publishers"+publisherRepository.count());
+        System.out.println("Publishers Number of Books"+publisher.getBooks().size());
 
 
 
